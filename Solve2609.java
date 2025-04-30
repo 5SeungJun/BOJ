f0ec0d916504ac4ca6e8d0743c8f4a941d0f2a0a
SeungJun <dhtmdwns319@naver.com>
2025-04-30 11:49:00 +0900

    최소공배수와 최대공약수

---
+++ Solve2609.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class Solve2609 {
+    public static void main(String [] args) throws IOException{
+        Buffredreader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
