10d7491f5091917e2592942467f79f374e4c5ded
ohsj <dhtmdwns319@naver.com>
2026-04-05 20:52:19 +0900

    아스키코드

---
+++ src/Solve11654.java
@@ -0,0 +1,10 @@
+package src;
+import java.io.*;
+public class Solve11654 {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        char c = input.charAt(0);
+        System.out.println((int)c);
+    }
+}
