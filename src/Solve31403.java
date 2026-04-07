073cd6414557189009d9f9aa499cbd88cb7ae3a3
ohsj <dhtmdwns319@naver.com>
2026-04-07 21:28:51 +0900

    A+B-C

---
+++ src/Solve31403.java
@@ -0,0 +1,14 @@
+package src;
+import java.io.*;
+public class Solve31403 {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String A = br.readLine();
+        String B = br.readLine();
+        String C = br.readLine();
+
+        System.out.println(Integer.parseInt(A)+Integer.parseInt(B)-Integer.parseInt(C));
+        System.out.println(Integer.parseInt(A+B)-Integer.parseInt(C));
+    }
+}
