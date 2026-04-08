aab75acb8d4840ed2b1a0466202843e09e048c6c
ohsj <dhtmdwns319@naver.com>
2026-04-08 14:47:36 +0900

    나머지

--- src/Solve3052.java
+++ src/Solve3052.java
@@ -1,4 +1,17 @@
 package src;
+import java.io.*;
+import java.util.*;
 
 public class Solve3052 {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<Integer,Integer> arr = new HashMap<>();
+
+        for(int i = 0; i < 10; i++){
+            int num = Integer.parseInt(br.readLine());
+            int temp = num % 42;
+            arr.putIfAbsent(temp, 1);
+        }
+        System.out.println(arr.size());
+    }
 }
