14729d0533ff2593f5a95db0028193de61e49bf0
ohsj <dhtmdwns319@naver.com>
2026-04-08 11:58:14 +0900

    단어의 개수(수정)

--- src/Solve1152.java
+++ src/Solve1152.java
@@ -3,10 +3,12 @@ import java.io.*;
 public class Solve1152 {
     public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String []input = br.readLine().split(" ");
-        if(input[0].equals("")){
-            System.out.println(input.length-1);
+        String str = br.readLine();
+        String [] temp = str.split(" ");
+        int count = 0;
+        for(String i : temp){
+            if(i != "") count++;
         }
-        else System.out.println(input.length);
+        System.out.println(count);
     }
 }
