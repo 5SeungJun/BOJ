4f6d4ce5c852e566490ca4f51008d037a6ceffd6
ohsj <dhtmdwns319@naver.com>
2026-04-23 11:53:31 +0900

    줄 세기(다른 방법)

--- src/Solve4806.java
+++ src/Solve4806.java
@@ -1,20 +1,27 @@
 package src;
 
-import java.util.NoSuchElementException;
-import java.util.Scanner;
-public class Solve4806 {
-    public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        int count = 0;
-        while(true){
-            try{
-                sc.nextLine();
-                count++;
-            }catch(NoSuchElementException e){
-                System.out.println(count);
-                return;
-            }
-        }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Solve4806 {
+    public static void main(String [] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+//        int count = 0;
+//        while(true){
+//            try{
+//                sc.nextLine();
+//                count++;
+//            }catch(NoSuchElementException e){
+//                System.out.println(count);
+//                return;
+//            }
+//        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        while(br.readLine() != null){
+            count++;
+        }
+        System.out.println(count);
     }
 }
