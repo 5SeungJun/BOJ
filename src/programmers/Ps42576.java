206cf5797c59a2892f6b5b4f1f2b5791f456925c
ohsj <dhtmdwns319@naver.com>
2026-04-23 15:57:21 +0900

    해시Lv1-완주하지 못한 선수

--- src/programmers/Ps42576.java
+++ src/programmers/Ps42576.java
@@ -3,14 +3,12 @@ import java.util.HashMap;
 public class Ps42576 {
     public String solution(String[] participant, String[] completion) {
         HashMap<String, Integer> map = new HashMap<>();
-
         for(String i : participant){
-            map.put(i, map.getOrDefault(i, 0) + 1);
+            map.put(i, map.getOrDefault(i, 0) + 1); //참여 명단에 이름(key) 나온만큼 value 증가
         }
         for(String i : completion){
-            map.put(i, map.get(i) - 1); //명단에 나온 이름 제거
+            map.put(i, map.get(i) - 1); //완주자 명단에 나온 이름 제거
         }
-
         for(String key : map.keySet()){
             if(map.get(key) > 0){
                 return key;
