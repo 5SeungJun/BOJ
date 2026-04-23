a4d3c21b68e88b52632503c8674ff6e0585985c9
ohsj <dhtmdwns319@naver.com>
2026-04-23 11:21:09 +0900

    직사각형

---
+++ src/Solve27323.java
@@ -0,0 +1,12 @@
+package src;
+
+import java.util.Scanner;
+
+public class Solve27323 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A*B);
+    }
+}
