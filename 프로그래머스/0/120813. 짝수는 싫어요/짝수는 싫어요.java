07aa61255ef62161e974c4d875576dba950f216d
5SeungJun <98459172+5SeungJun@users.noreply.github.com>
2026-06-15 19:57:52 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+
+        ArrayList<Integer> temp = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i % 2 == 0) continue;
+            temp.add(i);
+        }
+        
+        int[] answer = new int[temp.size()];
+        for(int i = 0; i < temp.size(); i++){
+            answer[i] = temp.get(i);
+        }
+        return answer;
+    }
+}
