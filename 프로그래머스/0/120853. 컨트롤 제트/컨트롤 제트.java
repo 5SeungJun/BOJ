902708836c33bbedfd4348ba833da741d3a64e2d
5SeungJun <98459172+5SeungJun@users.noreply.github.com>
2026-06-25 13:44:55 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.09 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String [] temp = s.split(" ");
+        for(int i = 0; i < temp.length; i++){
+            if(temp[i].equals("Z")){
+                answer -= Integer.parseInt(temp[i-1]);
+            }
+            else answer += Integer.parseInt(temp[i]);
+        }
+        return answer;
+    }
+}
