7f1b44435972d0a33a80b64796b2ac05843835ac
SeungJun_Oh <98459172+5SeungJun@users.noreply.github.com>
2026-08-14 23:38:27 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        boolean isFirst = true;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == ' '){
+                sb.append(c);
+                isFirst = true;
+            }
+            else if(isFirst){
+                sb.append(Character.toUpperCase(c));
+                isFirst = false;
+            }
+            else{
+                sb.append(Character.toLowerCase(c));
+                isFirst = false;
+            }
+        }
+        return sb.toString();
+    }
+}
